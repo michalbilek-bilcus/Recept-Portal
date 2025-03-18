8d6a811e0a390f906ec7e4ed222ecd4aef0baddc
michalbilek-bilcus <112864398+michalbilek-bilcus@users.noreply.github.com>
2025-03-18 11:43:57 +0100

    idk

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const mysql = require('mysql2');
 const bcrypt = require('bcryptjs');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors({
     origin: 'http://localhost:8080'
