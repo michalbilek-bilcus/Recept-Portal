50d481891c45bb41c36d873264b0b07f82af0da9
michalbilek-bilcus <112864398+michalbilek-bilcus@users.noreply.github.com>
2025-03-18 11:13:13 +0100

    server

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const mysql = require('mysql2');
@@ -11,10 +12,10 @@ app.use(cors({
 app.use(express.json());
 
 const connection = mysql.createConnection({
-    host: '127.0.0.1',
-    user: 'root',
-    password: '',
-    database: 'receptportal'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME 
 });
 
 connection.connect((err) => {
